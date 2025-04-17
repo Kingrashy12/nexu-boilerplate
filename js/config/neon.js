e1891405435b50014564ebac17827b6a7409a5f9
Rapheal Chizitere <www.rashyking05@gmail.com>
2025-04-17 17:44:38 +0100

    refactor: update query function to use sql.query method

--- js/config/neon.js
+++ js/config/neon.js
@@ -4,7 +4,9 @@ export const sql = neon(String(process.env.DATABASE_URL));
 
 export const query = async (queryText, values) => {
   try {
-    const result = values ? await sql(queryText, values) : await sql(queryText);
+    const result = values
+      ? await sql.query(queryText, values)
+      : await sql.query(queryText);
     return result;
   } catch (error) {
     throw new Error(`${error.message}`);
--- typescript/config/neon.ts
+++ typescript/config/neon.ts
@@ -2,12 +2,11 @@ import { neon } from "@neondatabase/serverless";
 
 export const sql = neon(String(process.env.DATABASE_URL));
 
-export const query = async <T = Record<string, any>>(
-  queryText: string,
-  values?: any[]
-) => {
+export const query = async (queryText: string, values?: any[]) => {
   try {
-    const result = values ? await sql(queryText, values) : await sql(queryText);
+    const result = values
+      ? await sql.query(queryText, values)
+      : await sql.query(queryText);
     return result;
   } catch (error) {
     throw new Error(`${(error as Error).message}`);
